e00b4539862b4bacc3b81ddf37a5a10604a7f820
高尚 <gaoshang212@hotmail.com>
2017-08-18 15:03:06 +0800

    adjust ignore

---
+++ lib/file.d.ts
@@ -0,0 +1,26 @@
+/// <reference types="node" />
+import * as fs from 'fs';
+export declare class file {
+    constructor();
+    exists(path: string): Promise<boolean>;
+    delete(path: string): Promise<void>;
+    readAllText(path: string): Promise<string>;
+    writeAllBytes(path: string, buffer: Buffer): Promise<void>;
+    stat(path: string): Promise<fs.Stats>;
+    writeAllText(path: string, content: string): Promise<void>;
+    writeByJson(path: string, obj: any): Promise<void>;
+    /**
+     * 检查文件夹读写权限
+     * @param path 目录
+     */
+    checkAccess(p_path: string): Promise<{
+        writeable: boolean;
+        readable: boolean;
+    }>;
+    /**
+     * 移动文件（重命名）
+     * @param oldPath
+     * @param newPath
+     */
+    move(oldPath: string, newPath: string): Promise<void>;
+}
---
+++ lib/index.d.ts
@@ -0,0 +1,3 @@
+import { file } from './file';
+declare var _default: file;
+export default _default;
