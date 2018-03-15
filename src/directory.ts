892d21d3b8a803efca530369d3010a0df5416cba
高尚 <gaoshang212@hotmail.com>
2018-03-15 14:12:58 +0800

    add directory delete and get getDirectories

--- src/directory.ts
+++ src/directory.ts
@@ -1,10 +1,30 @@
 import * as mkdirp from 'mkdirp';
+import * as fs from "fs";
+
+import * as rimraf from "rimraf";
+
 export class directory {
 
     public createDirectory(path: string): Promise<void> {
         let promise = new Promise<void>((resole, reject) => {
             mkdirp(path, err => !err ? resole() : reject(err));
         })
+        return promise;
+    }
+
+    public delete(path: string) {
+        let promise = new Promise<void>((resole, reject) => {
+            rimraf(path, err => !err ? resole() : reject(err));
+        });
+        return promise;
+    }
+
+    public getDirectories(path: string) {
+        let promise = new Promise<string[]>((resole, reject) => {
+            fs.readdir(path, (err, files) => {
+                !err ? resole(files) : reject(err);
+            })
+        });
         return promise;
     }
 }
